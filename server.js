0b55042c57876e2a31638b7fa1b84238072abbb0
majbaurrahmankhan <majbaurkhan@gmail.com>
2025-11-25 15:44:20 +0600

    adding .env

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const submissionRoutes = require('./routes/submissionRoutes');
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // MongoDB connection
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(() => console.log('MongoDB Connected'))
+//     .catch(err => console.error(err));
+
+// // Use submission routes
+// app.use('/api/submissions', submissionRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
@@ -13,6 +38,11 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error(err));
+
+// Root route
+app.get("/", (req, res) => {
+    res.send("Journal Server Running!");
+});
 
 // Use submission routes
 app.use('/api/submissions', submissionRoutes);
